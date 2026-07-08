32c2aa1e7d2ff7b0ba7b816339ffab0e84ad3c70
Sohel Ahmed <sohel@kali.forbit>
2026-07-08 18:12:49 +0600

    Change in Socket

--- hooks/useSocket.ts
+++ hooks/useSocket.ts
@@ -1,17 +1,50 @@
+// "use client";
+
+// import { useEffect } from "react";
+
+// import { socket } from "@/lib/socket";
+
+// export default function useSocket() {
+//   useEffect(() => {
+//     if (!socket.connected) {
+//       socket.connect();
+//     }
+
+//     return () => {};
+//   }, []);
+
+//   return socket;
+// }
+
 "use client";
 
 import { useEffect } from "react";
 
 import { socket } from "@/lib/socket";
+import { useAuthStore } from "@/store/authStore";
 
 export default function useSocket() {
+  const user = useAuthStore((state) => state.user);
+
   useEffect(() => {
-    if (!socket.connected) {
-      socket.connect();
+    socket.connect();
+
+    const handleConnect = () => {
+      if (user?._id) {
+        socket.emit("user:online", user._id);
+      }
+    };
+
+    socket.on("connect", handleConnect);
+
+    if (socket.connected) {
+      handleConnect();
     }
 
-    return () => {};
-  }, []);
+    return () => {
+      socket.off("connect", handleConnect);
+    };
+  }, [user?._id]);
 
   return socket;
 }
